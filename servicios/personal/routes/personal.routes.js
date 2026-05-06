b8e4ba35f5b30d7904ef029c01c4a2620e8b945f
DiegoLaguna-17 <diego.laguna@ucb.edu.bo>
2026-05-05 21:36:48 -0400

    Update personal.routes.js

--- servicios/personal/routes/personal.routes.js
+++ servicios/personal/routes/personal.routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { agregarPersonal, darBajaPersonal, obtenerPersonal, listarPersonal, modificarPersonal } = require('../controllers/personal.controller');
 
-router.post('/', agregarPersonal);
-router.get('/', listarPersonal);
+router.post('/agregar', agregarPersonal);
+router.get('/listar', listarPersonal);
 router.get('/:id', obtenerPersonal);
 router.put('/:id', modificarPersonal);
 router.patch('/:id/baja', darBajaPersonal);
